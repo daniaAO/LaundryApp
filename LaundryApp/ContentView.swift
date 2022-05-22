664df6d3e5eb4a678ccdd450a8141abd577db15a
Dania Abdulaziz <dania.abdulaziz@gmail.com>
2022-05-22 05:09:51 -0700

    Initial Commit

---
+++ LaundryApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LaundryApp
+//
+//  Created by Dania Alogla on 21/10/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LaundryApp/LaundryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaundryAppApp.swift
+//  LaundryApp
+//
+//  Created by Dania Alogla on 21/10/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct LaundryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
